9c974bf8efd6b3686464346a0ff0a73bb5619ed5
baharkose <baharkse17@gmail.com>
2024-03-19 11:56:50 +0300

    feat: dbconnection at index js and middleware express.json

--- index.js
+++ index.js
@@ -3,6 +3,10 @@
 // $ npm i mongoose
 //  npm i cookie-session
 
+// required modules
+// ! 1 gruplandırarak yazalım
+// - appin ayağa kalkması için en yukarıda requirementları yapalım. env ve portumuzu oladık. ve errorhandler tabi
+
 const express = require("express");
 const app = express();
 
@@ -10,4 +14,12 @@ require("dotenv").config();
 const PORT = process.env?.PORT || 8000;
 require("express-async-errors");
 
+// ! 2
+// Configurations - dbconnection
+// connect to db
 
+const { dbConnection } = require("./db");
+dbConnection();
+
+// ! 3 Şimdi sıra middleware kullanımında
+app.use(express.json());
