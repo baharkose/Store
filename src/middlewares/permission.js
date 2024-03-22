55cac80e3fae93998c70519d62fde27fec2ffcb0
baharkose <baharkse17@gmail.com>
2024-03-22 12:05:33 +0300

    feat: commentline

--- src/middlewares/permission.js
+++ src/middlewares/permission.js
@@ -1,5 +1,6 @@
 module.exports = {
   isAdmin: (req, res, next) => {
+    // ? req.user bizim userControl middlewareinden global olarak geliyor. Orda findOne ile bulduk onu.
     if (req.user && req.user.isAdmin) {
       console.log(req.user.isAdmin);
       next();
